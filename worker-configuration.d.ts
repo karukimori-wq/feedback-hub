cfb0d1df3a585f00e15310f344c51ff3db172045
karukimori-wq <karukimori@gmail.com>
2026-08-28 00:38:59 +0900

    Align Feedback Hub with AI Platform Core (worker-configuration.d.ts)

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,1 +1,8 @@
-interface Env { DB: D1Database; APP_NAME: string; CONTRACT_VERSION: string; }
+interface Env {
+  DB: D1Database;
+  AI_PLATFORM_CORE_SERVICE?: Fetcher;
+  AI_PLATFORM_CORE_BASE_URL?: string;
+  AI_PLATFORM_CORE_TOKEN?: string;
+  APP_NAME: string;
+  CONTRACT_VERSION: string;
+}
